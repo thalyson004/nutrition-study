7d1ce4612d18374f1afdd905eb2fa21418f58ed6
Thalyson <thalyon.silva@ifce.edu.br>
2023-11-07 09:46:05 -0300

    Add right nutrition method into Nutrition class

--- app/components/simple_types/nutrition.py
+++ app/components/simple_types/nutrition.py
@@ -35,5 +35,48 @@ class Nutrition:
     def getDictNutrition(state: State) -> dict:
         return Nutrition(state).data
 
+    def idealNutrition(eer: float = None) -> dict:
+        """Given the EER factor, retuns a dictionary with the right nutrition
+
+        Args:
+            eer (float): EER factor
+
+        Returns:
+            nutrition dictionary (dict): Dictionary with the right nutrition of each nutrient for a specific person with EER factor
+
+        """
+        # TODO: Define the right quantities
+        # FROM Eliseu 2020
+        # Negative means lower than
+        nutrients_quantiy = {
+            "ENERGIA_KCAL": eer if eer != None else 50,
+            "CHOTOT": eer * (55 / 100) if eer != None else 50,  # 55-75 eer
+            "PTN": eer * (15 / 100) if eer != None else 50,  # 10-15 eer
+            "LIP": (-1) * (eer * (30 / 100) if eer != None else 50),  # 15–30 eer
+            "FIBRA": 0,
+            "COLEST": (-1) * 300,
+            "CALCIO": 868,
+            "SODIO": 0,
+            "POTASSIO": 3510,
+            "FERRO": 6.8,
+            "MAGNESIO": 303,
+            "TIAMINA": 0.9,
+            "RIBOFLAVINA": 1,
+            "NIACINA": 11.5,
+            "PIRIDOXAMINA": 1.1,
+            "COBALAMINA": 2,
+            "VITC": 66.1,
+            "VITA_RAE": 560,
+            "COBRE": 0.7,
+            "FOLATO": 322,
+            "ZINCO": 8,
+            "FOSFORO": 649,
+        }
+
+        return nutrients_quantiy
+
+    def distance() -> dict:
+        return 0
+
     # def __setitem__(self, index, value):
     #     self.data[index] = value
